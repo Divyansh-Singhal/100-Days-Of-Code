33a48722b8abbefe4cec88c9ed22dcaca2dbd29c
Divyansh-Singhal <007singhaldivyansh@gmail.com>
2023-11-29 20:12:12 +0530

    day-128.java

---
+++ day-128.java
@@ -0,0 +1,25 @@
+public class BinarySearchTree {
+
+	// CREATE CLASS VARIABLE (ROOT) AND NODE CLASS HERE//
+	//                                                 //
+	//                                                 //
+	//                                                 //
+	//                                                 //
+	/////////////////////////////////////////////////////
+	  private Node root;
+ 
+    class Node {
+        public int value;
+        public Node left;
+        public Node right;
+ 
+        Node(int value) {
+            this.value = value;
+        }
+    }
+	
+	public Node getRoot() {
+        return root;
+    }
+
+}
