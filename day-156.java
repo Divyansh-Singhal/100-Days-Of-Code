537c783fe5b07f516c542ddaa7402a3c09e7e609
Divyansh-Singhal <007singhaldivyansh@gmail.com>
2023-12-29 20:12:12 +0530

    day-157

--- day-156.java
+++ day-156.java
@@ -1,3 +1,16 @@
-public class day-156 {
-    
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+      if(n == 1)
+        return true;
+      else if(n <= 0)
+       return false;
+        while(n > 1){
+          if(n % 2 == 0){
+            n = n / 2;
+          }else{
+            return false;
+          }
+        }
+        return true;
+    }
 }
