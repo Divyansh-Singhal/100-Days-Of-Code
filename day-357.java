7e666cccaa2ff5d3194c62b44ffd689185814bd9
Divyansh-Singhal <007singhaldivyansh@gmail.com>
2024-07-05 08:00:00 +0530

    day-348

---
+++ day-357.java
@@ -0,0 +1,17 @@
+private int size = 7;
+    private Node[] dataMap;
+ 
+    class Node {
+        String key;
+        int value;
+        Node next;
+ 
+        Node(String key, int value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+ 
+    public HashTable() {
+        dataMap = new Node[size];
+    }
